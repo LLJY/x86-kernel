47803de9fb1823bc3142a3ea813a05de38c087a3
Arjan van de Ven <arjan@linux.intel.com>
2015-02-11 16:05:23 -0600

    bootstats: add printk's to measure boot time in more detail
    Few distro-tweaks to add printk's to visualize boot time better
    
    Author:    Arjan van de Ven <arjan@linux.intel.com>
    
    Signed-off-by: Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>

--- arch/x86/kernel/alternative.c
+++ arch/x86/kernel/alternative.c
@@ -1632,7 +1632,9 @@ void __init alternative_instructions(void)
 	 * Then patch alternatives, such that those paravirt calls that are in
 	 * alternatives can be overwritten by their immediate fragments.
 	 */
+	printk("clr: Applying alternatives\n");
 	apply_alternatives(__alt_instructions, __alt_instructions_end);
+	printk("clr: Applying alternatives done\n");
 
 	/*
 	 * Now all calls are established. Apply the call thunks if
