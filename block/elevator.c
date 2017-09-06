2591889a0884044e37e5bcb1f31e9103ed893450
LLJY <lucasleeeeeeeee@gmail.com>
2017-09-06 18:56:24 +0200

    Revert "ZEN: Implement zen-tune v4.12"
    This reverts commit eea78d60d4a2baa9df4ec614c3f3f637c0b24618.

--- block/elevator.c
+++ block/elevator.c
@@ -233,7 +233,6 @@ int elevator_init(struct request_queue *q, char *name)
 					e = elevator_get("bfq-mq", false);
 				#else
 				e = elevator_get("mq-deadline", false);
-				#endif
 			if (!e)
 				return 0;
 		} else
