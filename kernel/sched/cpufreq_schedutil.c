06aaf3dff752115b3b159765a1423c8b7c39eb93
Alfred Chen <cchalpha@gmail.com>
2018-03-23 12:40:54 +0800

    pds: [Sync] 794a56ebd9a5 sched/cpufreq: Change the worker kthread to SCHED_DEADLINE

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -483,8 +483,12 @@ static int sugov_kthread_create(struct sugov_policy *sg_policy)
 	struct task_struct *thread;
 	struct sched_attr attr = {
 		.size		= sizeof(struct sched_attr),
+#ifdef CONFIG_SCHED_PDS
+		.sched_policy	= SCHED_FIFO,
+#else
 		.sched_policy	= SCHED_DEADLINE,
 		.sched_flags	= SCHED_FLAG_SUGOV,
+#endif
 		.sched_nice	= 0,
 		.sched_priority	= 0,
 		/*
