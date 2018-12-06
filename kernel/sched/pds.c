acff2d8c60e64eba21eff1ff6b0134e43d59862a
Alfred Chen <cchalpha@gmail.com>
2018-12-06 15:23:39 +0000

    pds: [Sync] a7fe5190c03f cpuidle: menu: Remove get_loadavg() from the performance multiplier

--- kernel/sched/pds.c
+++ kernel/sched/pds.c
@@ -2674,14 +2674,6 @@ unsigned long nr_iowait_cpu(int cpu)
 	return atomic_read(&cpu_rq(cpu)->nr_iowait);
 }
 
-void get_iowait_load(unsigned long *nr_waiters, unsigned long *load)
-{
-	struct rq *rq = this_rq();
-
-	*nr_waiters = atomic_read(&rq->nr_iowait);
-	*load = rq->nr_running;
-}
-
 /*
  * IO-wait accounting, and how its mostly bollocks (on SMP).
  *
