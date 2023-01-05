22c72018f545751cfd8fb0c79988a572bfdfd2db
Arjan van de Ven <arjan.van.de.ven@intel.com>
2023-01-05 16:52:33 +0000

    add networking support for powerbump

--- net/core/dev.c
+++ net/core/dev.c
@@ -163,6 +163,7 @@
 #include <net/page_pool/memory_provider.h>
 #include <net/rps.h>
 #include <linux/phy_link_topology.h>
+#include <linux/powerbump.h>
 
 #include "dev.h"
 #include "devmem.h"
@@ -6141,6 +6142,7 @@ int netif_receive_skb(struct sk_buff *skb)
 	int ret;
 
 	trace_netif_receive_skb_entry(skb);
+	give_power_bump(BUMP_FOR_NETWORK);
 
 	ret = netif_receive_skb_internal(skb);
 	trace_netif_receive_skb_exit(ret);
@@ -6165,6 +6167,7 @@ void netif_receive_skb_list(struct list_head *head)
 
 	if (list_empty(head))
 		return;
+	give_power_bump(BUMP_FOR_NETWORK);
 	if (trace_netif_receive_skb_list_entry_enabled()) {
 		list_for_each_entry(skb, head, list)
 			trace_netif_receive_skb_list_entry(skb);
