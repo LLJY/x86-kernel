62e7b34a3c749bb9f85f7a1bf4ce4899a230950f
Alfred Chen <cchalpha@gmail.com>
2024-03-22 18:39:10 +0800

    Project-C v6.8-r3

--- kernel/sched/alt_core.c
+++ kernel/sched/alt_core.c
@@ -76,7 +76,7 @@ __read_mostly int sysctl_resched_latency_warn_once = 1;
 #define sched_feat(x)	(0)
 #endif /* CONFIG_SCHED_DEBUG */
 
-#define ALT_SCHED_VERSION "v6.8-r2"
+#define ALT_SCHED_VERSION "v6.8-r3"
 
 /*
  * Compile time debug macro
