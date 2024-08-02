d13db5bc4e4e7d43c5be4b255ac203aec52c0404
Alfred Chen <cchalpha@gmail.com>
2024-08-02 14:28:16 +0000

    Project-C v6.10-r1

--- kernel/sched/alt_core.c
+++ kernel/sched/alt_core.c
@@ -76,7 +76,7 @@ __read_mostly int sysctl_resched_latency_warn_once = 1;
 #define sched_feat(x)	(0)
 #endif /* CONFIG_SCHED_DEBUG */
 
-#define ALT_SCHED_VERSION "v6.10-r0"
+#define ALT_SCHED_VERSION "v6.10-r1"
 
 /* rt_prio(prio) defined in include/linux/sched/rt.h */
 #define rt_task(p)		rt_prio((p)->prio)
